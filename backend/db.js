091ce0b44f1a2051f00d0662da1de96a0a490c41
Janek <eunho.moon@gmail.com>
2023-07-13 19:28:39 +0900

    update

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,6 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_ROOT_PASSWORD,
-  // database: MYSQL_DATABASE,
   prort: MYSQL_PORT,
 });
 exports.pool = pool;
